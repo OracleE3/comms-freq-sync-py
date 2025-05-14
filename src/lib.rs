20ea24a9ddfae4edab28776d65bf57ad15288a91
Daniel Cain <daniel.cain@pnnl.gov>
2025-05-14 11:18:19 -0700

    Got pysdr comparison notebook working

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,45 @@
-use std::f32::consts::PI;
-
-use log::{debug};
-use num::{complex::Complex32, Complex};
-use phastft::{planner::Direction, fft_32};
-use pyo3::pyfunction;
+use log::debug;
+use num_complex::Complex32;
+use phastft::fft_32_with_opts_and_plan;
+use phastft::options::Options;
+use phastft::planner::{Direction, Planner32};
+use pyo3::prelude::*;
 
 #[pyfunction]
 pub fn coarse_freq_correction_psk(
     iq_data: Vec<Complex32>,
     sample_rate: f32,
-    psk_order: usize
-) -> Vec<Complex32> {
+    psk_order: u32
+) -> (Vec<Complex32>, f32) {
     // Taken from https://pysdr.org/content/sync.html#coarse-frequency-synchronization
     let mut reals: Vec<f32> = Vec::with_capacity(iq_data.len());
     let mut imags: Vec<f32> = Vec::with_capacity(iq_data.len());
-    let mut corr_iq_data: Vec<Complex32> = Vec::with_capacity(iq_data.len());
-    for iq in iq_data {
-        let c = iq.pow(psk_order);
-        reals.push(c.re());
-        imags.push(c.im());
+    let mut corr_iq_data: Vec<Complex32> = iq_data.clone();
+    for iq in iq_data.iter() {
+        let c = iq.powu(psk_order);
+        reals.push(c.re);
+        imags.push(c.im);
     }
-    let psd = fft_32(&mut reals, &mut imags, Direction::Forward);
-    let freq_offset = psd.max();
-    debug!("Freq offset: {?}", freq_offset);
+    let mut planner = Planner32::new(reals.len(), Direction::Forward);
+    let opts = Options::default();
+    fft_32_with_opts_and_plan(&mut reals, &mut imags, &opts, &mut planner);
+    let offset_index = reals
+        .iter()
+        .zip(imags.iter())
+        .enumerate()
+        .map(|(index, (&r, &i))| (index, Complex32::new(r, i).norm()))
+        .max_by(|(_, a), (_, b)| a.total_cmp(b))
+        .map(|(index, _)| index)
+        .expect("Unable to get max value of PSD (freq offset)");
+    // let freq_offset = (-sample_rate / 2.0) + ((offset_index as f32) * sample_rate / iq_data.len() as f32);
+    let freq_offset = (offset_index as f32) * sample_rate / iq_data.len() as f32;
+    debug!("Offset index {:?} - Freq offset: {:?}", offset_index, freq_offset);
     let ts: f32 = 1.0 / sample_rate;
-    for i in 0..corr_iq_data.len() {
-        corr_iq_data[i] *= Complex32::new(0.0, 2.0 * psk_order * PI * ).exp()
+    for i in 0..iq_data.len() {
+        let shift = Complex32::new(0.0, -2.0 * (psk_order as f32) * core::f32::consts::PI * freq_offset * (i as f32) * ts).exp();
+        corr_iq_data[i] = iq_data[i] * shift;
     }
-    corr_iq_data
+    (corr_iq_data, freq_offset)
 }
 
 #[pyfunction]
@@ -68,8 +80,9 @@ pub fn costas_loop(
 
 /// A Python module implemented in Rust.
 #[pymodule]
-#[pyo3(name = "_lib")]
-fn src(_py: Python, m: &PyModule) -> PyResult<()> {
+#[pyo3(name = "comms_freq_sync_py")]
+fn src(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(coarse_freq_correction_psk, m)?)?;
     m.add_function(wrap_pyfunction!(costas_loop, m)?)?;
     Ok(())
 }
